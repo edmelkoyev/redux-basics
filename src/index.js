a137ae21fb1d6ac3f00fed986c1eb626004e06a4
Edward Melkoyev <edmelkoyev@gmail.com>
2017-12-23 16:33:16 +0300

    Initial code state

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Store from './store';
+
+const inotialState = { count: 0 };
+
+function updateState(state, action){
+    switch (action.type) {
+        case 'INCREMENT': return {count: state.count + action.amount};
+        case 'DECREMENT': return {count: state.count - action.amount};
+        case 'RESET': return { count: 0 };
+        default: return state;
+    }
+}
+
+const incrementAction = {type: "INCREMENT", amount: 1};
+const decrementAction = {type: "DECREMENT", amount: 1};
+const resetAction = {type: "RESET"};
+
+const store = new Store(updateState, inotialState);
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { count: 0 };
+
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    componentDidMount(){
+        store.subscribe(() => this.forceUpdate());
+    }
+
+    increment() {
+        store.update(incrementAction);
+    }
+
+    decrement() {
+        store.update(decrementAction);
+    }
+
+    reset() {
+        store.update(resetAction);
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <span className="count">{store.state.count}</span>
+
+                <div className="buttons">
+                    <button className="decrement" onClick={this.decrement}>-</button>
+                    <button className="reset" onClick={this.reset}>R</button>
+                    <button className="increment" onClick={this.increment}>+</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
